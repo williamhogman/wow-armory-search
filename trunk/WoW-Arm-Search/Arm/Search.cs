7701a2d59eeb6a3b47d3fddfc817740d1c6b75fe
pajdeg <pajdeg@fff1a950-d33e-0410-a0bb-b958a1c7b885>
2007-11-14 19:57:48 +0000

    ADDED: New Downloading Function BUG: New Downloading Function does not work

--- trunk/WoW-Arm-Search/Arm/Search.cs
+++ trunk/WoW-Arm-Search/Arm/Search.cs
@@ -20,14 +20,48 @@ namespace Arm
 
         private void btn_go_Click(object sender, EventArgs e)
         {
-            WebClient Client = new WebClient();
-       Client.Headers["User-agent"]="Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9a7) Gecko/2007080210 GranParadiso/3.0a7";
-            //Client.DownloadFile("http://eu.wowarmory.com/character-sheet.xml?r="+tbx_rel.Text+"&n="+tbx_name.Text, "armory.xml");
-            MessageBox.Show(Client.DownloadString("http://eu.wowarmory.com/character-sheet.xml?r=" + tbx_rel.Text + "&n=" + tbx_name.Text));
-            viewer View_frm= new viewer();
+            Download_Char("", "");
+viewer View_frm = new viewer();
+
         View_frm.Show();
 
         
         }
+
+
+        public string Download_Char(string name,string server){
+        string result = "";
+try
+{
+
+
+WebRequest request = WebRequest.Create("http://www.google.com");
+//request.Headers["User-Agent"] = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9";
+// Send the 'HttpWebRequest' and wait for response.
+HttpWebResponse response = (HttpWebResponse)request.GetResponse(); System.IO.Stream stream = response.GetResponseStream();
+System.Text.Encoding ec = System.Text.Encoding.GetEncoding("utf-8");
+System.IO.StreamReader reader = new System.IO.StreamReader(stream, ec);
+char [] chars = new Char[256];
+int count = reader.Read(chars, 0, 256);
+while(count > 0)
+{
+string str = new String(chars, 0, 256);
+result = result + str;
+count = reader.Read(chars, 0, 256);
+}
+response.Close();
+stream.Close();
+reader.Close();
+
+}
+catch(Exception exp)
+{
+string str = exp.Message;
+
+}
+return result;
+
+        }
+
     }
 }
