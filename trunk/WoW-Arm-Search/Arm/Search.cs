185d107e538bc95f5c317e34356e66197d763758
pajdeg <pajdeg@fff1a950-d33e-0410-a0bb-b958a1c7b885>
2007-11-15 13:23:06 +0000

    ADDED: Downloading of files (TEMP)

--- trunk/WoW-Arm-Search/Arm/Search.cs
+++ trunk/WoW-Arm-Search/Arm/Search.cs
@@ -6,9 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.Web;
 using System.Net;
-
+using System.Web;
+using System.IO;
 namespace Arm
 {
     public partial class Search : Form
@@ -20,8 +20,8 @@ namespace Arm
 
         private void btn_go_Click(object sender, EventArgs e)
         {
-            Download_Char("", "");
-viewer View_frm = new viewer();
+    Save(  Download_Char("Tarendor", "Aggramar"));
+      viewer View_frm = new viewer();     
 
         View_frm.Show();
 
@@ -29,38 +29,49 @@ viewer View_frm = new viewer();
         }
 
 
+        private void Save(string data) {
+            MessageBox.Show(data);
+            
+            StreamWriter stream = new StreamWriter("armory.xml",false);
+       
+            stream.Write(data);
+
+            stream.Close();
+        
+
+
+        }
+
         public string Download_Char(string name,string server){
+         //   return Arm_dl("http://wowarmory.com/character-sheet.xml?r="+server+"&n="+name);
+            return Arm_dl("http://wowarmory.com/character-sheet.xml?r=Tichondrius&n=Hammura");
+        }
+
+
+
+
+        public string Arm_dl(string adr) { 
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(adr);
+        req.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2) Gecko/20070219 Firefox/2.0.0.2";
+        HttpWebResponse response = (HttpWebResponse)req.GetResponse();
         string result = "";
-try
-{
+        System.Text.Encoding ec = System.Text.Encoding.GetEncoding("utf-8");
+        System.IO.StreamReader reader = new System.IO.StreamReader(response.GetResponseStream(), ec);
+        char[] chars = new Char[256];
+        int count = reader.Read(chars, 0, 256);
+ 
+        while (count > 0)
+        {
+            string str = new String(chars, 0, 256);
+            result = result + str;
+            count = reader.Read(chars, 0, 256);
+        }
+        response.Close();
+     
+        reader.Close();
 
-
-WebRequest request = WebRequest.Create("http://www.google.com");
-//request.Headers["User-Agent"] = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9";
-// Send the 'HttpWebRequest' and wait for response.
-HttpWebResponse response = (HttpWebResponse)request.GetResponse(); System.IO.Stream stream = response.GetResponseStream();
-System.Text.Encoding ec = System.Text.Encoding.GetEncoding("utf-8");
-System.IO.StreamReader reader = new System.IO.StreamReader(stream, ec);
-char [] chars = new Char[256];
-int count = reader.Read(chars, 0, 256);
-while(count > 0)
-{
-string str = new String(chars, 0, 256);
-result = result + str;
-count = reader.Read(chars, 0, 256);
-}
-response.Close();
-stream.Close();
-reader.Close();
-
-}
-catch(Exception exp)
-{
-string str = exp.Message;
-
-}
-return result;
-
+        return result;
+        
         }
 
     }
