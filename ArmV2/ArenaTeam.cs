9a3b01575c69267cb7a2ff784aa2c1f6a04695a8
pajdeg <pajdeg@fff1a950-d33e-0410-a0bb-b958a1c7b885>
2007-11-15 21:34:14 +0000

    ArmoryV2 Added

---
+++ ArmV2/ArenaTeam.cs
@@ -0,0 +1,35 @@
+﻿/* Copyright Nameless Gnome
+ * This file is part of WoW Armory Search.
+
+    WoW Armory Search is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WoW Armory Search is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WoW Armory Search.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arm
+{
+    public class ArenaTeam
+    {
+        public int Size;
+        public string Name;
+        public int Win;
+        public int Lose;
+        public int Rating;
+
+        public List<BasicPlayerInfo> members;
+
+    }
+}
